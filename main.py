2f0476150669fecb3d92495d13e5e63b42a4074f
Carlos Infante <kuva5008@gmail.com>
2025-09-10 12:14:08 +0200

    bookbot complete

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
-from stats import get_num_words
-from stats import get_num_chars
+import sys
+from stats import sorted_list
 
 
 def main(relative_path):
-    print(get_num_words(relative_path)
-          )
-    print(get_num_chars(relative_path))
+    if not relative_path:
+        print("No file path provided.")
+        sys.exit(1)
+    return sorted_list(relative_path)
 
 
-main('books/frankenstein.txt')
+# 'books/frankenstein.txt'
+if sys.argv and len(sys.argv) != 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+else:
+    main(sys.argv[1])
--- stats.py
+++ stats.py
@@ -5,7 +5,7 @@ def get_num_words(relative_path):
         words = text.split()
         num_words = len(words)
 
-    print(f"{num_words} words found in the document")
+    return f" Found {num_words} total words"
 
 
 def get_num_chars(relative_path):
@@ -17,6 +17,30 @@ def get_num_chars(relative_path):
                 new_dict[char] += 1
             else:
                 new_dict[char] = 1
-    print(new_dict)
-
     return new_dict
+
+
+def sort_on(items):
+    return items["num"]
+
+
+def sorted_list(relative_path):
+    my_dict = get_num_chars(relative_path)
+    my_list = []
+
+    for key, value in my_dict.items():
+        new_dict = {"char": key, "num": value}
+        my_list.append(new_dict)
+
+    my_list.sort(key=sort_on, reverse=True)
+
+    new_list = []
+    for dic in my_list:
+        if dic["char"].isalpha():
+            new_dict = {dic["char"]: dic["num"]}
+            new_list.append(new_dict)
+
+    print(get_num_words(relative_path))
+    for dic in new_list:
+        for key, value in dic.items():
+            print(f"- {key}: {value}")
